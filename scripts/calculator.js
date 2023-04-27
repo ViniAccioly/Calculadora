6f5a639f73ccef9a61101572e8492875edca646c
Vinicius Costa Accioly <119104725+ViniAccioly@users.noreply.github.com>
2023-04-27 11:02:14 -0300

    Add files via upload

---
+++ scripts/calculator.js
@@ -0,0 +1,7 @@
+/* Porque programar Orientada a objeto ?
+Possui praticidade, reutilização e abstração == otimização de código
+*/
+
+// MVC = Model View Controller
+
+window.calculator = new CalController; 
